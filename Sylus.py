7a0cb09c95b0f5203786d744c37398a5193a129f
Cissy19Sylus <127461047+Cissy19Sylus@users.noreply.github.com>
2025-04-12 18:55:04 +0800

    Sylus.py

--- Sylus.py
+++ Sylus.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title('秦彻生日快乐')
-  if st.button("Send balloons!"):
+    if st.button("Send balloons!"):
     st.balloons()
 st.badge("秦彻生日快乐！", color="red")
-  if st.checkbox('秦彻生日快乐！'):
+    if st.checkbox('秦彻生日快乐！'):
       video_file = open('./秦彻PV.mp4', 'rb')
       video_bytes = video_file.read()
 
