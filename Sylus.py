bdb445573d5e97953fa2db4f9c126ea0e501d01f
Cissy19Sylus <127461047+Cissy19Sylus@users.noreply.github.com>
2025-04-12 20:37:42 +0800

    Update Sylus.py

--- Sylus.py
+++ Sylus.py
@@ -127,8 +127,10 @@ st.markdown(
 
 # 视频展示区
 st.markdown('<h3>🎬 生日PV 🎬</h3>', unsafe_allow_html=True)
-      video_file = open('秦彻PV.mp4', 'rb')
-      video_bytes = video_file.read()
+
+video_file = open('秦彻PV.mp4', 'rb')  # 确保这一行的缩进与前后代码一致
+video_bytes = video_file.read()
+st.video(video_bytes)
 st.title('清晰度致歉，Github只能上传25MB的文件')
 
 # 留言区
